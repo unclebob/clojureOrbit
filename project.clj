bce25f82905e5a38bf5ddccbd5b1ccf9e01512cf
Karsten Lang <karstenlang@gmail.com>
2010-06-03 18:02:43 +0200

    using lein is too easy

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject clojureOrbit "0.1"
+  :description "simple Orbital Simulator written in Clojure."
+  :url "http://github.com/unclebob/clojureOrbit"
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]]
+  :dev-dependencies [[swank-clojure "1.2.0-SNAPSHOT"]
+		     [leiningen/lein-swank "1.2.0-SNAPSHOT"]])
+
