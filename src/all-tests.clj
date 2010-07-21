8cbb24f919a01bb2c4f128a7cf4cee6158cde803
unclebob <unclebob@objectmentor.com>
2010-07-21 12:07:36 -0500

    housekeeping

--- src/all-tests.clj
+++ src/all-tests.clj
@@ -1,5 +1,3 @@
 (ns orbit (:use clojure.test))
 (require 'physics.vector-test)
-(require 'physics.position-test)
-(require 'physics.object-test)
 (run-tests 'physics)
--- src/physics/physics.clj
+++ src/physics/physics.clj
@@ -1,5 +1,3 @@
 (ns physics)
 (require ['physics.vector :as 'vector])
-(require ['physics.position :as 'position])
-(require ['physics.object :as 'object])
 
