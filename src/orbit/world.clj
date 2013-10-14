565c0791e7c3148710a9e16d65698ecfd21fec2d
Robert C. Martin <unclebob@cleancoder.com>
2013-10-14 18:07:35 -0500

    New fading trails

--- src/orbit/world.clj
+++ src/orbit/world.clj
@@ -9,34 +9,33 @@
 
 (import-static java.awt.event.KeyEvent VK_LEFT VK_RIGHT VK_UP VK_DOWN)
 
-(def center   (position/make 500 500))
-(def running? (atom true))
+(def center (position/make 500 500))
 
-(defstruct controls :magnification :center :trails :clear)
+(defstruct controls :magnification :center :trails :clear )
 
 (defn size-by-mass [{m :mass}]
   (+ 0 (Math/sqrt m)))
 
 (defn color-by-mass [{m :mass}]
   (condp > m
-    1  Color/black
-    2  (Color. 210 105 30)
-    5  Color/red
+    1 Color/black
+    2 (Color. 210 105 30)
+    5 Color/red
     10 (Color. 107 142 35)
     20 Color/magenta
     40 Color/blue
     (Color. 255 215 0)))
 
 (defn draw-object [g obj controls]
-  (let [mag        (:magnification controls)
+  (let [mag (:magnification controls)
         sun-center (:center controls)
-        x-offset   (- (first center) (* mag (first sun-center)))
-        y-offset   (- (last center) (* mag (last sun-center)))
-        x          (+ x-offset (* mag (first (:position obj))))
-        y          (+ y-offset (* mag (last (:position obj))))
-        s          (max 2 (* mag (size-by-mass obj)))
-        half-s     (/ s 2)
-        c          (color-by-mass obj)]
+        x-offset (- (first center) (* mag (first sun-center)))
+        y-offset (- (last center) (* mag (last sun-center)))
+        x (+ x-offset (* mag (first (:position obj))))
+        y (+ y-offset (* mag (last (:position obj))))
+        s (max 2 (* mag (size-by-mass obj)))
+        half-s (/ s 2)
+        c (color-by-mass obj)]
     (.setColor g c)
     (.fillOval g (- x half-s) (- y half-s) s s)))
 
@@ -49,43 +48,48 @@
       (draw-object g obj controls))
     (.clearRect g 0 0 1000 20)
     (.drawString g (format "Objects: %d, Magnification: %4.3g"
-                           (count world)
-                           (:magnification controls)) 20 20)))
+                     (count world)
+                     (:magnification controls)) 20 20)))
+
+(defn update-world-history [world]
+  (let [new-world (conj world (object/update-all (last world)))]
+    (if (> (count new-world) 200)
+      (let [r (rand-int 100)]
+        (vec (concat (take r new-world) (drop (inc r) new-world))))
+      new-world)))
 
 (defn update-world [world controls]
-  (swap! world object/update-all))
+  (swap! world update-world-history))
 
 (defn magnify [factor controls world]
   (dosync
-    (let [sun-position (:position (find-sun @world))
+    (let [sun-position (:position (find-sun (last @world)))
           new-mag (* factor (:magnification @controls))]
       (swap! controls assoc
-             :magnification new-mag
-             :center        sun-position
-             :clear         true))))
+        :magnification new-mag
+        :center sun-position))))
 
-(defn reset-screen-state [controls]
-  (swap! controls assoc :clear false))
-
-(defn toggle-trail [controls]
-  (swap! controls assoc :trails (-> @controls :trails not)))
+(defn clear-trails [world]
+  (vec (drop (dec (count world)) world)))
 
 (defn handle-key [c world controls]
   (condp = c
-    \q     (swap! running? not)
-    \+     (magnify 1.1 controls world)
-    \-     (magnify 0.9 controls world)
-    \space (magnify 1.0 controls world)
-    \t     (toggle-trail controls)
+    \q (System/exit 0)
+    \+ (magnify 1.3 controls world)
+    \- (magnify 0.9 controls world)
+    \_ (magnify 0.7 controls world)
+    \= (magnify 1.1 controls world)
+    \c (magnify 1.0 controls world)
+    \space (do
+             (swap! world clear-trails)
+             (magnify 1.0 controls world))
     nil))
 
 (defn world-panel [frame world controls]
   (proxy [JPanel ActionListener KeyListener] []
     (paintComponent [g]
-      (when (or (:clear @controls) (not (:trails @controls)))
-        (proxy-super paintComponent g))
-      (draw-world g @world @controls)
-      (reset-screen-state controls))
+      (proxy-super paintComponent g)
+      (doseq [w @world] (draw-world g w @controls)))
     (keyPressed [e]
       (handle-key (.getKeyChar e) world controls)
       (.repaint this))
@@ -98,9 +102,9 @@
   (- (rand (* 2 n)) n))
 
 (defn random-velocity [p sun]
-  (let [sp        (:position sun)
-        sd        (position/distance p sp)
-        v         (Math/sqrt (/ 1 sd))
+  (let [sp (:position sun)
+        sd (position/distance p sp)
+        v (Math/sqrt (/ 1 sd))
         direction (vector/rotate90 (vector/unit (vector/subtract p sp)))]
     (vector/scale direction (+ (rand 0.01) (* v 13.5)))))
 
@@ -124,11 +128,11 @@
 
 (defn world-frame []
   (let [controls (atom (struct-map controls
-                        :magnification 1.0
-                        :center center
-                        :trails false
-                        :clear false))
-        world (atom (create-world))
+                         :magnification 1.0
+                         :center center
+                         :trails false
+                         :clear false))
+        world (atom [(create-world)])
         frame (JFrame. "Orbit")
         panel (world-panel frame world controls)]
     (doto panel
@@ -140,12 +144,9 @@
       (.setVisible true)
       (.setDefaultCloseOperation JFrame/DISPOSE_ON_CLOSE))
     (future
-     (while @running?
-       (.repaint panel)
-       (Thread/sleep 40)))
-    (future
-     (while @running?
-            (time (update-world world @controls))))))
+      (while true
+        (update-world world @controls)
+        (.repaint panel)))))
 
 (defn run-world []
   (world-frame))
